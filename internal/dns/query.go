5dd24fb152f8715834c5610d9ceab76c55d17391
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-06-15 14:11:20 +0200

    DNS QueryPacket unmarshaling (refactor needed)

--- internal/dns/query.go
+++ internal/dns/query.go
@@ -1,22 +1,98 @@
 package dns
 
-type Query struct {
-	Name  string
-	Type  uint16
-	Class uint16
-}
+import (
+	"github.com/SergeyCherepiuk/dns-go/internal/utils"
+)
 
 type QueryPacket struct {
-	Header  Header
-	Queries []Query
+	Header    Header
+	Questions []Question
 }
 
 // TODO: Not implemented
 func MarshalQueryPacket(packet QueryPacket) []byte {
-	return nil
+	bytes := make([]byte, 0)
+
+	headerBytes := MarshalHeader(packet.Header)
+	bytes = append(bytes, headerBytes[:]...)
+
+	for _, question := range packet.Questions {
+		_ = question
+		// TODO
+	}
+
+	return bytes
 }
 
-// TODO: Not implemented
-func UnmarshalQeuryPacket(bytes []byte) (QueryPacket, error) {
-	return QueryPacket{}, nil
+// TODO: Refactor this nightmare
+func UnmarshalQueryPacket(bytes []byte) QueryPacket {
+	j := uint16(0)
+
+	var (
+		headerBytes = [HeaderSize]byte(bytes[j : j+HeaderSize])
+		header      = UnmarshalHeader(headerBytes)
+	)
+	j += HeaderSize
+
+	queryPacket := QueryPacket{
+		Header:    header,
+		Questions: make([]Question, header.QuestionSectionSize),
+	}
+
+	for i := range header.QuestionSectionSize {
+		var (
+			domain      = make([]byte, 0)
+			domainIndex = j
+		)
+
+		for {
+			size := uint16(bytes[domainIndex])
+			if domainIndex == j {
+				j += 1
+			}
+			domainIndex += 1
+
+			if size&0b11000000 == 0b11000000 {
+				pointerBytes := [2]byte{byte(size) & 0b00111111, bytes[domainIndex]}
+				pointer := utils.BytesToUint16(pointerBytes)
+				domainIndex = pointer
+
+				j += 1
+
+				continue
+			}
+
+			if size == 0 {
+				break
+			}
+
+			domain = append(domain, bytes[domainIndex:domainIndex+size]...)
+			domain = append(domain, '.')
+
+			if domainIndex == j {
+				j += size
+			}
+			domainIndex += size
+		}
+
+		var (
+			questionTypeBytes = [2]byte{bytes[j], bytes[j+1]}
+			questionType      = QuestionType(utils.BytesToUint16(questionTypeBytes))
+		)
+		j += 2
+
+		var (
+			questionClassBytes = [2]byte{bytes[j], bytes[j+1]}
+			questionClass      = QuestionClass(utils.BytesToUint16(questionClassBytes))
+		)
+		j += 2
+
+		queryPacket.Questions[i] = Question{
+			Domain: string(domain),
+			Type:   questionType,
+			Class:  questionClass,
+		}
+	}
+
+	return queryPacket
 }
---
+++ internal/dns/query_test.go
@@ -0,0 +1,145 @@
+package dns
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/SergeyCherepiuk/dns-go/internal/utils"
+)
+
+func TestUnmarshalQueryPacketOneQuestion(t *testing.T) {
+	bytes := []byte{
+		// Header
+		0x12, 0x34, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+
+		// Question 1 (google.com.)
+		0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03,
+		0x63, 0x6f, 0x6d, 0x00, 0x00, 0x01, 0x00, 0x01,
+	}
+
+	expectedQueryPackcet := QueryPacket{
+		Header: Header{
+			ID:                           0x1234,
+			PacketType:                   PacketTypeQuery,
+			Opcode:                       OpcodeQuery,
+			AuthoritativeAnswer:          false,
+			Truncated:                    false,
+			RecursionDesired:             true,
+			RecursionAvailable:           false,
+			AuthenticData:                true,
+			CheckingDisabled:             false,
+			ResponseCode:                 ResponseCodeNoError,
+			QuestionSectionSize:          1,
+			AnswerSectionSize:            0,
+			AuthorityRecordsSectionSize:  0,
+			AdditionalRecordsSectionSize: 0,
+		},
+		Questions: []Question{
+			{"google.com.", QuestionTypeA, QuestionClassIN},
+		},
+	}
+
+	actualQueryPacket := UnmarshalQueryPacket(bytes)
+
+	if !reflect.DeepEqual(actualQueryPacket, expectedQueryPackcet) {
+		entries := utils.Diff(actualQueryPacket, expectedQueryPackcet)
+		t.Fatal(entries.String())
+	}
+}
+
+func TestUnmarshalQueryPacketTwoQuestion(t *testing.T) {
+	bytes := []byte{
+		// Header
+		0x12, 0x34, 0x01, 0x20, 0x00, 0x02, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+
+		// Question 1 (google.com.)
+		0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03,
+		0x63, 0x6f, 0x6d, 0x00, 0x00, 0x01, 0x00, 0x01,
+
+		// Question 2 (mx.google.com.)
+		0x02, 0x6d, 0x78, 0xc0, 0x0c, 0x00, 0x0f, 0x00,
+		0x01,
+	}
+
+	expectedQueryPackcet := QueryPacket{
+		Header: Header{
+			ID:                           0x1234,
+			PacketType:                   PacketTypeQuery,
+			Opcode:                       OpcodeQuery,
+			AuthoritativeAnswer:          false,
+			Truncated:                    false,
+			RecursionDesired:             true,
+			RecursionAvailable:           false,
+			AuthenticData:                true,
+			CheckingDisabled:             false,
+			ResponseCode:                 ResponseCodeNoError,
+			QuestionSectionSize:          2,
+			AnswerSectionSize:            0,
+			AuthorityRecordsSectionSize:  0,
+			AdditionalRecordsSectionSize: 0,
+		},
+		Questions: []Question{
+			{"google.com.", QuestionTypeA, QuestionClassIN},
+			{"mx.google.com.", QuestionTypeMX, QuestionClassIN},
+		},
+	}
+
+	actualQueryPacket := UnmarshalQueryPacket(bytes)
+
+	if !reflect.DeepEqual(actualQueryPacket, expectedQueryPackcet) {
+		entries := utils.Diff(actualQueryPacket, expectedQueryPackcet)
+		t.Fatal(entries.String())
+	}
+}
+
+func TestUnmarshalQueryPacketThreeQuestion(t *testing.T) {
+	bytes := []byte{
+		// Header
+		0x12, 0x34, 0x01, 0x20, 0x00, 0x03, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+
+		// Question 1 (google.com.)
+		0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03,
+		0x63, 0x6f, 0x6d, 0x00, 0x00, 0x01, 0x00, 0x01,
+
+		// Question 2 (mx.google.com.)
+		0x02, 0x6d, 0x78, 0xc0, 0x0c, 0x00, 0x0f, 0x00,
+		0x01,
+
+		// Question 3 (com.)
+		0xc0, 0x13, 0x00, 0x01, 0x00, 0x01,
+	}
+
+	expectedQueryPackcet := QueryPacket{
+		Header: Header{
+			ID:                           0x1234,
+			PacketType:                   PacketTypeQuery,
+			Opcode:                       OpcodeQuery,
+			AuthoritativeAnswer:          false,
+			Truncated:                    false,
+			RecursionDesired:             true,
+			RecursionAvailable:           false,
+			AuthenticData:                true,
+			CheckingDisabled:             false,
+			ResponseCode:                 ResponseCodeNoError,
+			QuestionSectionSize:          3,
+			AnswerSectionSize:            0,
+			AuthorityRecordsSectionSize:  0,
+			AdditionalRecordsSectionSize: 0,
+		},
+		Questions: []Question{
+			{"google.com.", QuestionTypeA, QuestionClassIN},
+			{"mx.google.com.", QuestionTypeMX, QuestionClassIN},
+			{"com.", QuestionTypeA, QuestionClassIN},
+		},
+	}
+
+	actualQueryPacket := UnmarshalQueryPacket(bytes)
+
+	if !reflect.DeepEqual(actualQueryPacket, expectedQueryPackcet) {
+		entries := utils.Diff(actualQueryPacket, expectedQueryPackcet)
+		t.Fatal(entries.String())
+	}
+}
