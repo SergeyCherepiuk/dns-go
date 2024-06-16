56af70d5a4cf3b0702caae375a48d6bc456edf83
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-06-16 15:46:53 +0200

    Test the value of DNS header constants

--- internal/dns/header_test.go
+++ internal/dns/header_test.go
@@ -6,6 +6,30 @@ import (
 	"github.com/SergeyCherepiuk/dns-go/internal/utils"
 )
 
+func TestHeaderConstants(t *testing.T) {
+	entries := make(utils.DiffEntries, 0)
+
+	entries = append(entries, utils.Diff(HeaderSize, 12)...)
+
+	entries = append(entries, utils.Diff(PacketTypeQuery, 0)...)
+	entries = append(entries, utils.Diff(PacketTypeResponse, 1)...)
+
+	entries = append(entries, utils.Diff(OpcodeQuery, 0)...)
+	entries = append(entries, utils.Diff(OpcodeIQuery, 1)...)
+	entries = append(entries, utils.Diff(OpcodeStatus, 2)...)
+
+	entries = append(entries, utils.Diff(ResponseCodeNoError, 0)...)
+	entries = append(entries, utils.Diff(ResponseCodeFormatError, 1)...)
+	entries = append(entries, utils.Diff(ResponseCodeServerFailure, 2)...)
+	entries = append(entries, utils.Diff(ResponseCodeNameError, 3)...)
+	entries = append(entries, utils.Diff(ResponseCodeNotImplemented, 4)...)
+	entries = append(entries, utils.Diff(ResponseCodeRefused, 5)...)
+
+	if len(entries) > 0 {
+		t.Fatal(entries.String())
+	}
+}
+
 func TestMarshalHeaderQueryPacket(t *testing.T) {
 	header := Header{
 		ID:                           0x4edb,
@@ -25,8 +49,7 @@ func TestMarshalHeaderQueryPacket(t *testing.T) {
 	}
 
 	expectedBytes := [HeaderSize]byte{
-		0x4e, 0xdb, 0x01, 0x20,
-		0x00, 0x01, 0x00, 0x00,
+		0x4e, 0xdb, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00,
 	}
 
@@ -55,8 +78,7 @@ func TestMarshalHeaderResponsePacket(t *testing.T) {
 	}
 
 	expectedBytes := [HeaderSize]byte{
-		0x4e, 0xdb, 0x81, 0x80,
-		0x00, 0x01, 0x00, 0x01,
+		0x4e, 0xdb, 0x81, 0x80, 0x00, 0x01, 0x00, 0x01,
 		0x00, 0x00, 0x00, 0x00,
 	}
 
@@ -70,8 +92,7 @@ func TestMarshalHeaderResponsePacket(t *testing.T) {
 
 func TestUnmarshalHeaderQueryPacket(t *testing.T) {
 	bytes := [HeaderSize]byte{
-		0x4e, 0xdb, 0x01, 0x20,
-		0x00, 0x01, 0x00, 0x00,
+		0x4e, 0xdb, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00,
 	}
 
@@ -102,8 +123,7 @@ func TestUnmarshalHeaderQueryPacket(t *testing.T) {
 
 func TestUnmarshalHeaderResponsePacket(t *testing.T) {
 	bytes := [HeaderSize]byte{
-		0x4e, 0xdb, 0x81, 0x80,
-		0x00, 0x01, 0x00, 0x01,
+		0x4e, 0xdb, 0x81, 0x80, 0x00, 0x01, 0x00, 0x01,
 		0x00, 0x00, 0x00, 0x00,
 	}
 
