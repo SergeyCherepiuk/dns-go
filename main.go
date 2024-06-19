0039ddeee0b13360254ffa709c66eec43bb7387e
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-06-19 12:08:50 +0200

    Stub resolver: DNS client quering "google.com." IPv4 address

--- main.go
+++ main.go
@@ -1,3 +1,68 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/SergeyCherepiuk/dns-go/internal/dns"
+)
+
+func main() {
+	var (
+		listenAddr  = net.UDPAddr{IP: net.IPv4(0, 0, 0, 0), Port: 0}
+		receiveAddr = net.UDPAddr{IP: net.IPv4(8, 8, 8, 8), Port: 53}
+	)
+
+	conn, err := net.DialUDP("udp", &listenAddr, &receiveAddr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := dns.Packet{
+		Header: dns.Header{
+			ID:                  0x1234,
+			PacketType:          dns.PacketTypeQuery,
+			Opcode:              dns.OpcodeQuery,
+			RecursionDesired:    true,
+			QuestionSectionSize: 1,
+		},
+		Questions: []dns.Question{
+			{
+				Domain: "google.com.",
+				Type:   dns.QuestionTypeA,
+				Class:  dns.QuestionClassIN,
+			},
+		},
+	}
+
+	queryBytes := dns.MarshalPacket(query)
+	n, err := conn.Write(queryBytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if n != len(queryBytes) {
+		log.Fatalf("not all bytes have been read by the server (%d out of %d)", n, len(queryBytes))
+	}
+
+	responseBytes := make([]byte, 512)
+	n, err = conn.Read(responseBytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response := dns.UnmarshalPacket(responseBytes[:n])
+
+	for _, answer := range response.Answers {
+		fmt.Printf(
+			"%s -> %v (ttl: %d)\n",
+			answer.Domain,
+			net.IP(answer.Data),
+			answer.Ttl,
+		)
+	}
+}
