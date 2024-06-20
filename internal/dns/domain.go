6ebd813d5a02cc224caeef802d997ec0b9790d3b
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2024-06-20 11:01:40 +0200

    DNS domain name parsing: Minor changes

--- internal/dns/domain.go
+++ internal/dns/domain.go
@@ -32,11 +32,11 @@ func MarshalDomain(domain string, lookup map[int]string) []byte {
 func UnmarshalDomain(bytes []byte, lookup map[int]string) (string, int) {
 	var (
 		domain    []byte
-		bytesRead uint16
+		bytesRead int
 	)
 
 	for {
-		size := uint16(bytes[bytesRead])
+		size := int(bytes[bytesRead])
 		bytesRead += 1
 
 		if size == 0 {
@@ -45,12 +45,12 @@ func UnmarshalDomain(bytes []byte, lookup map[int]string) (string, int) {
 
 		if size&0b11000000 == 0b11000000 {
 			pointerBytes := [2]byte{byte(size) & 0b00111111, bytes[bytesRead]}
-			pointer := utils.BytesToUint16(pointerBytes)
 			bytesRead += 1
 
-			lookedupDomain := []byte(lookup[int(pointer)])
-			domain = append(domain, lookedupDomain...)
+			pointer := int(utils.BytesToUint16(pointerBytes))
+			lookedupDomain := []byte(lookup[pointer])
 
+			domain = append(domain, lookedupDomain...)
 			break
 		}
 
@@ -59,5 +59,5 @@ func UnmarshalDomain(bytes []byte, lookup map[int]string) (string, int) {
 		bytesRead += size
 	}
 
-	return string(domain), int(bytesRead)
+	return string(domain), bytesRead
 }
